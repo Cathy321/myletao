e683b2ff0d6d144947bdf165025a4af25a1653ba
shasha <474135778@qq.com>
2018-08-07 13:26:14 +0800

    登录静态页面

---
+++ public/back/js/login.js
@@ -0,0 +1,43 @@
+$(function() {
+  $('#form').bootstrapValidator({
+    //配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    //配置字段
+    fields: {
+      //校验用户名
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2-6位之间'
+          }
+        }
+      },
+      //校验密码
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: '密码长度在6-12位之间'
+          }
+        }
+      }
+    }
+  })
+})
